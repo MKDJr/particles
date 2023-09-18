9312f4ebb504a919cd6c2a2ccb9d99b594bf99d9
Michael Dawson <61061178+MKDJr@users.noreply.github.com>
2023-09-18 19:40:37 -0400

    cleaning

---
+++ target/debug/deps/rust_project-f3c869a6923c3a79.d
@@ -0,0 +1,5 @@
+/Users/michaeldawson/github-projects/rust/rust-project/target/debug/deps/rust_project-f3c869a6923c3a79: src/main.rs
+
+/Users/michaeldawson/github-projects/rust/rust-project/target/debug/deps/rust_project-f3c869a6923c3a79.d: src/main.rs
+
+src/main.rs:
