4b1ec036df9eb9f206ebd04f18dd73e91675a49a
Michael Dawson <61061178+MKDJr@users.noreply.github.com>
2023-09-29 18:30:58 -0400

    lists

---
+++ lists/target/debug/deps/lists-04df8a169a93768c.d
@@ -0,0 +1,5 @@
+/Users/michaeldawson/github-projects/rust/lists/target/debug/deps/lists-04df8a169a93768c.rmeta: src/lib.rs
+
+/Users/michaeldawson/github-projects/rust/lists/target/debug/deps/lists-04df8a169a93768c.d: src/lib.rs
+
+src/lib.rs:
---
+++ lists/target/debug/deps/lists-8d2f661f1b3870f2.d
@@ -0,0 +1,5 @@
+/Users/michaeldawson/github-projects/rust/lists/target/debug/deps/lists-8d2f661f1b3870f2.rmeta: src/lib.rs
+
+/Users/michaeldawson/github-projects/rust/lists/target/debug/deps/lists-8d2f661f1b3870f2.d: src/lib.rs
+
+src/lib.rs:
